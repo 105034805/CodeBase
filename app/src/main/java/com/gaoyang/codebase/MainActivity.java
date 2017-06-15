34aa896121c30c35999bbe7d1e9651b6651a7c5d
Gaoyang <yang.gao@nestia.com>
2017-06-15 15:32:52 +0800

    one

--- app/src/main/java/com/gaoyang/codebase/MainActivity.java
+++ app/src/main/java/com/gaoyang/codebase/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); 
         setContentView(R.layout.activity_main);
 
         mButton = (Button) findViewById(R.id.button);
